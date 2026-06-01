c4046b67033f3b8e26bef5e42ffcfa64924486c3
Vsevolod Sazonov <legoman056@gmail.com>
2026-06-01 18:31:24 +0700

    create interactor

--- search/src/main/kotlin/domain/interactor/SearchInteractor.kt
+++ search/src/main/kotlin/domain/interactor/SearchInteractor.kt
@@ -1,13 +1,18 @@
 package domain.interactor
 
+import androidx.paging.PagingData
+import domain.models.FilterDto
 import domain.models.FoundPetInfo
+import domain.models.PetUiPreview
+import kotlinx.coroutines.flow.Flow
 import model.InternetStatus
 import ui.model.Response
 import ui.viewModel.SpottedAnimalData
 
 interface SearchInteractor {
 
-
+    suspend fun loadMissAnnouncementPage(filterDto: FilterDto) : Flow<PagingData<PetUiPreview>>
+    suspend fun loadFindAnnouncementPage(filterDto: FilterDto) : Flow<PagingData<PetUiPreview>>
 
     suspend fun getInfoAboutPet(
         id: String,
--- search/src/main/kotlin/domain/interactor/SearchInteractorImpl.kt
+++ search/src/main/kotlin/domain/interactor/SearchInteractorImpl.kt
@@ -1,7 +1,11 @@
 package domain.interactor
 
+import androidx.paging.PagingData
+import domain.models.FilterDto
 import domain.models.FoundPetInfo
+import domain.models.PetUiPreview
 import domain.repository.SearchRepository
+import kotlinx.coroutines.flow.Flow
 import model.InternetStatus
 import ui.model.Response
 import ui.viewModel.SpottedAnimalData
@@ -9,6 +13,13 @@ import ui.viewModel.SpottedAnimalData
 class SearchInteractorImpl(
     private val repository: SearchRepository
 ) : SearchInteractor {
+    override suspend fun loadMissAnnouncementPage(filterDto: FilterDto): Flow<PagingData<PetUiPreview>> {
+        return repository.loadMissAnnouncementPage(filterDto)
+    }
+
+    override suspend fun loadFindAnnouncementPage(filterDto: FilterDto): Flow<PagingData<PetUiPreview>> {
+        return repository.loadFindAnnouncementPage(filterDto)
+    }
 
     override suspend fun getInfoAboutPet(
         id: String,
