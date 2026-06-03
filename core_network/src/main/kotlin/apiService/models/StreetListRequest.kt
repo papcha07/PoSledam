bb0556eddcd6f8ba627b9863eb0cd17fc8665d1f
Vsevolod Sazonov <legoman056@gmail.com>
2026-06-03 13:40:02 +0700

    add new values in StreetListRequest

--- core_network/src/main/kotlin/apiService/models/StreetListRequest.kt
+++ core_network/src/main/kotlin/apiService/models/StreetListRequest.kt
@@ -3,5 +3,8 @@ package apiService.models
 data class StreetListRequest(
     val lastDateTime: String?,
     val from: String? = null,
-    val type: Int? = null
+    val type: Int? = null,
+    val centerRadius : Int,
+    val searchCenterLatitude: Double,
+    val searchCenterLongitude: Double
 )
